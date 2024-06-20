89358a45940a062a697391930e496a85396dd3bb
Aidan Cooney <acooney613@gmail.com>
2024-06-20 00:18:07 -0600

    updates

--- js/header.js
+++
@@ -1,8 +0,0 @@
-let appHeader = `
-    <header style="background-color: darkgreen; position: fixed; top: 0; left: 0; right:0; width: 100%; padding: 20px 0; text-align: right; color: white;">
-        <nav>
-            <a title = 'Welcome Page' href="welcome.html" style="color: white; text-decoration: none; margin: 0 15px;">Welcome</a>
-        </nav>
-    </header>
-`;
-document.getElementById("app-header").innerHTML = appHeader;
