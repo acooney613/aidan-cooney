f116fe809ae02b45b4057cd05e7173e82b738edd
Aidan Cooney <acooney613@gmail.com>
2024-06-24 19:47:26 -0600

    updates 6/24/24

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,3 +2,28 @@
 function openProjectOne() {
     window.open('index.html', '_blank');
 }
+
+// *** Add additional projects here opening the appropriate html *** //
+
+// Expand-skills icon functionality
+document.addEventListener('DOMContentLoaded', function() {
+    const expandIcon = document.querySelector('.expand-icon');
+    const hiddenSkills = document.querySelectorAll('.skill-hidden');
+    expandIcon.addEventListener('click', function() {
+        if (expandIcon.innerText == 'more...') {
+            expandIcon.innerText = '...less';
+            
+            hiddenSkills.forEach(function(skill) {
+                skill.style.display = 'block';
+            });
+        }
+
+        else {
+            expandIcon.innerText = 'more...';
+
+            hiddenSkills.forEach(function(skill) {
+                skill.style.display = 'none';
+            });
+        }
+    });
+});
